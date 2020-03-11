c2eef5d9ccb2451440049e3552ff853042b1d278
ryanbegley96 <ryanbegleyphys@gmail.com>
2020-03-11 22:21:07 +0000

    Implemented mag calc with z, finihs + process results

--- MagnitudeModel.py
+++ MagnitudeModel.py
@@ -18,27 +18,29 @@ class MagnitudeModel(object):
 
         self.magnitude = self.calculateMagnitude()
 
+
     def resampleSpectrum(self):
 
         samplingRange = (np.min(self.transCurve.wavelength),
                           np.max(self.transCurve.wavelength))
         samplingWavelengthBool = ((self.spectrum.wavelength>samplingRange[0])
                                   & (self.spectrum.wavelength<samplingRange[1]))
-        #produce arrays of value over the range of the filter.
-        # self.samplingWavelength = self.spectrum.wavelength[samplingWavelengthBool]
-        # self.sampledTransCurve = self.transCurve.returnInterpolationFunc(
-        #                                          self.samplingWavelength)
-        # self.sampledFlux = self.spectrum.w_flux[samplingWavelengthBool]
-        # self.sampledFluxAB = self.spectrumAB.w_flux[samplingWavelengthBool]
-        dict = {"Wavelength":self.spectrum.wavelength[samplingWavelengthBool],
+        # produce arrays of value over the range of the filter.
+        self.samplingWavelength = self.spectrum.wavelength[samplingWavelengthBool]
+        self.sampledTransCurve = self.transCurve.returnInterpolationFunc()(
+                                                 self.samplingWavelength)
+        self.sampledFlux = self.spectrum.w_flux[samplingWavelengthBool]
+        self.sampledFluxAB = self.spectrumAB.w_flux[samplingWavelengthBool]
+        result = {"Wavelength":self.spectrum.wavelength[samplingWavelengthBool],
                 "Flux":self.spectrum.w_flux[samplingWavelengthBool],
                 "FluxAB":self.spectrumAB.w_flux[samplingWavelengthBool],
                 }
-        return dict
+        return result
+
 
     def calculateMagnitude(self):
         integ = np.sum(self.sampledFlux*self.samplingWavelength*
             self.sampledTransCurve)
         norm = np.sum(self.sampledFluxAB*self.samplingWavelength*
             self.sampledTransCurve)
-        return integ/norm#self.spectrum.f_fluxToAB(integ/norm)
+        return -2.5*np.log10(integ/norm)
--- ObservationModel.py
+++ ObservationModel.py
@@ -1,4 +1,5 @@
 import copy
+import time
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -10,26 +11,29 @@ class ObservationModel():
 
     def __init__(self,zRange,transDict,spectraDict):
 
-        self.zRange = np.arange(*zRange,4)
+        self.zRange = np.arange(*zRange,0.1)
         self.transCurves = [TransmissionCurve(file)
                             for file in transDict['inputFile']]
         self.spectrumAB = FakeSpectrum(0.0,1E2,3E4,0.0,False)
         self.RestSpectrum = FakeSpectrum(**spectraDict)
         self.ObservedSpectra = self.makeObservedSpectra()
-
+        
     def makeObservedSpectra(self):
-        ObservedSpectra = []
+        ObservedSpectra = {}
         for z in self.zRange:
             observedSpectrum = copy.deepcopy(self.RestSpectrum)
             observedSpectrum.wavelength = observedSpectrum.wavelength * (1.0+z)
-            ObservedSpectra.append(observedSpectrum)
+            ObservedSpectra[str(z)] = observedSpectrum
         return ObservedSpectra
 
     def runObservations(self):
-        self.magnitudes = []
-        for spectrum in self.ObservedSpectra:
-            magModel = MagnitudeModel(spectrum,self.spectrumAB,self.transCurves[0])
-            self.magnitudes.append(magModel.magnitude)
+        self.magnitudes = {}
+        for zKey,spectrum in self.ObservedSpectra.items():
+            magList = []
+            for transCurve in self.transCurves:
+                magModel = MagnitudeModel(spectrum,self.spectrumAB,transCurve)
+                magList.append(magModel.magnitude)
+            self.magnitudes[zKey] = magList
 
 def main():
     spectraDict = {"nuIndex":0.0,
@@ -38,36 +42,69 @@ def main():
                    "normalisationMag":25,
                    "activateIGM":True}
 
-    transDict = {"inputFile":["TransmissionCurveFiles/Subaru_HSC.g_filter.dat",
+    transDict = {"inputFile":["TransmissionCurveFiles/Subaru_HSC.Y.dat",
                               "TransmissionCurveFiles/Paranal_VISTA.Y.dat"]}
 
-    zRange = [0,5]
+    zRange = [0,10]
     model = ObservationModel(zRange,transDict,spectraDict)
-    model.runObservations()
-    print(model.magnitudes)
 
-    obj1 = model.ObservedSpectra[0]
-    obj2 = model.ObservedSpectra[1]
+    model.runObservations()
+    print("mag :", model.magnitudes)
+
+    obj1 = model.ObservedSpectra['0.0']
     filter1 = model.transCurves[0]
     filter2 = model.transCurves[1]
 
-    fig,axs = plt.subplots()
-    plt.plot(obj1.wavelength/1E4,obj1.f_fluxToAB(obj1.f_flux))
-    plt.plot(obj2.wavelength/1E4,obj2.f_fluxToAB(obj2.f_flux))
-    axs.set_xlabel(r'$\lambda/\mu$m')
-    axs.set_ylabel(r'$m_{AB}$')
-    axs.set_xlim(xmin=0.,xmax=3.0)
-    axs.set_ylim(ymax=30,ymin=22)
-    axs.invert_yaxis()
+    # plt.ion()
+    # fig,axs = plt.subplots()
+    # plot1, = plt.plot(obj1.wavelength/1E4,obj1.f_fluxToAB(obj1.f_flux),'k')
+    # axs.set_xlabel(r'$\lambda/\mu$m')
+    # axs.set_ylabel(r'$m_{AB}$')
+    # axs.set_xlim(xmin=0.,xmax=3.0)
+    # axs.set_ylim(ymax=30,ymin=22)
+    # axs.invert_yaxis()
 
-    ax2 = axs.twinx()
-    # ax2.plot(filter1.wavelength/1E4,filter1.transmission,c='r')
-    ax2.fill_between(filter1.wavelength/1E4,filter1.transmission,edgecolor='r',facecolor='r',alpha=0.5)
-    # ax2.fill_between(filter1.wavelength/1E4,filter1.transmission,edgecolor='r',facecolor='r',alpha=0.5)
-    ax2.set_ylabel("Transmission/%")
-    ax2.set_ylim(ymin=0,ymax=1)
+    # ax2 = axs.twinx()
+    # ax2.fill_between(filter1.wavelength/1E4,filter1.transmission,edgecolor='r',facecolor='r',alpha=0.5,label="HSC-Y")
+    # ax2.fill_between(filter2.wavelength/1E4,filter2.transmission,edgecolor='b',facecolor='b',alpha=0.5,label="UVISTA-Y")
+    # ax2.set_ylabel("Transmission/%")
+    # ax2.set_ylim(ymin=0,ymax=1)
+    # ax2.legend(fontsize='10')
+
+    # for _,spectrum in model.ObservedSpectra.items():
+    #     plt.pause(0.1)
+    #     plot1.set_xdata(spectrum.wavelength/1E4)
+    #     fig.canvas.draw()
+
+    # colourList = 
+    # fig,axs = plt.subplots()
+
 
     plt.show()
 
 if __name__ == '__main__':
     main()
+
+"""
+def test(spectraDict,transDict):
+    model = ObservationModel([0,1],{"inputFile":["testTransmissionCurve.txt"]},spectraDict)
+    magModel = MagnitudeModel(model.RestSpectrum, model.spectrumAB, 
+                              model.transCurves[0])
+    print("calc : ",magModel.magnitude)
+    # print("sampled vals : ",magModel.sampledFlux)
+    
+    fig,axs = plt.subplots()
+    plt.plot(model.RestSpectrum.wavelength/1E4,model.RestSpectrum.f_fluxToAB(model.RestSpectrum.f_flux))
+    plt.plot(model.spectrumAB.wavelength/1E4,model.spectrumAB.f_fluxToAB(model.spectrumAB.f_flux))
+    axs.set_xlabel(r'$\lambda/\mu$m')
+    axs.set_ylabel(r'$m_{AB}$')
+    axs.set_xlim(xmin=0.,xmax=3.0)
+    # axs.set_ylim(ymax=30,ymin=22)
+    filter1 = model.transCurves[0]
+    axs.invert_yaxis()
+    ax2 = axs.twinx()
+    # ax2.plot(filter1.wavelength/1E4,filter1.transmission,c='r')
+    ax2.fill_between(filter1.wavelength/1E4,filter1.transmission,edgecolor='r',facecolor='r',alpha=0.5)
+    ax2.set_ylabel("Transmission/%")
+    ax2.set_ylim(ymin=0,ymax=1)
+"""
