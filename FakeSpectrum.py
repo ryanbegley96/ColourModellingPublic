d3d03263112f1789043f83b963094e5f252e1c65
rbeg <rbeg@loyal.roe.ac.uk>
2020-03-11 10:11:15 +0000

    updating laptop local

--- FakeSpectrum.py
+++ FakeSpectrum.py
@@ -3,21 +3,29 @@ import matplotlib.pyplot as plt
 
 class FakeSpectrum(object):
 
-    def __init__(self,nuIndex, lowerLambda=100,upperLambda=30000):
+    def __init__(self,nuIndex=0, lowerLambda=100,upperLambda=30000,
+                 normalisationMag=25,activateIGM=True):
         """
         Instance with attributes wavelength & flux
         """
         self.cSpeed = 3.0E+8
-        self.nuIndex = nuIndex
-        self.lambdaIndex = nuIndex-2.0
+        self.normalisationWavelength = 25000.0 #2.5microns
 
-        self.wavelength = np.linspace(lowerLambda,upperLambda,100)
+        self.nuIndex = nuIndex
+        self.nuNorm = self.setNuNorm(normalisationMag)
+
+        self.lambdaIndex = nuIndex-2.0
+        self.lambdaNorm = self.setLambdaNorm(normalisationMag)
+
+        self.wavelength = np.arange(lowerLambda,upperLambda,10)
         self.frequency = self.fluxdensityUnitSwap(self.wavelength)
 
         self.f_flux = self.spectrumNuModel(self.frequency,self.nuIndex)
         self.w_flux = self.spectrumLambdaModel(self.wavelength,
                                                self.lambdaIndex)
-        self.IGM_absorption()
+
+        if activateIGM:
+            self.IGM_absorption()
 
     def IGM_absorption(self):
         #Flux should be set to 0 at lambda<1.216um or freq>2.4653985e+14Hz
@@ -26,27 +34,39 @@ class FakeSpectrum(object):
         self.w_flux[IGM_bool] = (9.1201082e-50 * self.cSpeed
                                 /self.wavelength[IGM_bool]**2.0)
 
-    def fluxdensityUnitSwap(self,fluxdensity,currentUnit='wavelength'):
+    def setLambdaNorm(self,normalisationMag):
+        w_flux = self.fluxdensityUnitSwap( self.ABTof_flux(normalisationMag),
+                                    'frequency', self.normalisationWavelength )
+        return w_flux / self.normalisationWavelength**self.lambdaIndex
+
+    def setNuNorm(self,normalisationMag): #3.6307805E-30
+        self.normalisationNu = 1E10*self.cSpeed/self.normalisationWavelength
+        return (self.ABTof_flux(normalisationMag)
+                / self.normalisationNu**self.nuIndex )
+
+    def fluxdensityUnitSwap(self,fluxdensity,currentUnit='wavelength',
+                            wavelength=None):
         """
         Converts from F_lam [erg/s/cm2/A] <-> F_nu [erg/s/cm2/Hz]
         """
+        if wavelength == None:
+            wavelength = self.wavelength
+
         if currentUnit == 'wavelength':  #fv=3.34x10^-19 * l**2 * fl
-            return fluxdensity * self.wavelength**2.0 * 1.0E-10 / self.cSpeed
+            return fluxdensity * wavelength**2.0 * 1.0E-10 / self.cSpeed
         elif currentUnit == 'frequency': #fl=3x10^18 * fv / l**2
-            return fluxdensity * 1.0E+10 * self.cSpeed / self.wavelength**2.0
+            return fluxdensity * 1.0E+10 * self.cSpeed / wavelength**2.0
 
     def spectrumNuModel(self,frequency,nuIndex):
         """
         Spectrum modelled as fv=a*frequency**nuIndex
         """
-        self.nuNorm = 3.6307805E-30
         return self.nuNorm*frequency**nuIndex
 
     def spectrumLambdaModel(self,wavelength,lambdaIndex):
         """
         Spectrum modelled as fl=b*wavelength**alpha, where alpha=nuIndex-2
         """
-        self.lambdaNorm = 1.08537E-11
         return self.lambdaNorm*wavelength**lambdaIndex
 
     def f_fluxToAB(self,f_flux):
@@ -55,8 +75,10 @@ class FakeSpectrum(object):
     def w_fluxToAB(self,w_flux):
         return -2.5*np.log10(self.fluxdensityUnitSwap(w_flux))-48.6
 
+    def ABTof_flux(self,mag):
+        return 10.0**(-(mag+48.6)/2.5)
 
-    def spectrumMagPlotter(self):
+    def showSpectrum(self):
         _,axs = plt.subplots()
         axs.plot(self.wavelength/1E4,self.f_fluxToAB(self.f_flux))
         axs.set_xlabel(r'$\lambda/\mu$m')
@@ -68,7 +90,7 @@ class FakeSpectrum(object):
 def main():
     nuIndex = 0.0
     sed = FakeSpectrum(nuIndex)
-    sed.spectrumMagPlotter()
+    sed.showSpectrum()
 
 if __name__ == '__main__':
     main()
--- TransmissionCurve.py
+++ TransmissionCurve.py
@@ -1,25 +1,92 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy.interpolate import interp1d
 
 class TransmissionCurve(object):
 
     def __init__(self, inputFile):
 
         self.transmissionFile = inputFile
-        self.data = self.extractContents(inputFile)
+        self.wavelength,self.transmission = self.extractContents(inputFile)
 
     def extractContents(self,inputFile):
         """
         Assuming ascii file format from SVO
         """
-        return np.loadtxt(inputFile)
+        data = np.loadtxt(inputFile)
+        return data[:,0],data[:,1]
 
+    def returnInterpolationFunc(self):
+        """
+        Creates interpolation function which can be used to sample.
+        """
+        self.interpFunc = interp1d(self.wavelength,self.transmission,
+                                    kind='cubic')
+        return self.interpFunc
+
+    def showTransmissionCurve(self):
+        _,axs = plt.subplots()
+        axs.plot(self.wavelength/1E4,self.transmission)
+        axs.set_xlabel(r"$\lambda/\mu$m")
+        axs.set_ylabel("Transmission/%")
+        axs.set_ylim(ymin=0)
+        return axs
 
 def main():
     inputFile = "TransmissionCurveFiles/Subaru_HSC.g_filter.dat"
     transCurve = TransmissionCurve(inputFile)
-    print(transCurve.transmissionFile)
-    print(transCurve.data)
+
+    interpFunc = transCurve.returnInterpolationFunc()
+    xData = np.linspace(np.min(transCurve.wavelength),
+                        np.max(transCurve.wavelength),1000,endpoint=True)
+    yData = interpFunc(xData)
+    axs = transCurve.showTransmissionCurve()
+    axs.plot(xData/1E4,yData)
+    plt.show()
 
 if __name__ == '__main__':
     main()
+
+
+
+
+
+"""
+Experimental Code:
+>Needs high order > 100 to fit decently but this is time intensive.
+
+from symfit import parameters,variables,sin,cos,Fit
+
+def fourier_series(x, f, n=0):
+
+    #Returns a symbolic fourier series of order `n`.
+
+    #:param n: Order of the fourier series.
+    #:param x: Independent variable
+    #:param f: Frequency of the fourier series
+
+    # Make the parameter objects for all the terms
+    a0, *cos_a = parameters(','.join(['a{}'.format(i) for i in range(0, n + 1)]))
+    sin_b = parameters(','.join(['b{}'.format(i) for i in range(1, n + 1)]))
+    # Construct the series
+    series = a0 + sum(ai * cos(i * f * x) + bi * sin(i * f * x)
+    for i, (ai, bi) in enumerate(zip(cos_a, sin_b), start=1))
+    return series
+
+x, y = variables('x, y')
+w, = parameters('w')
+model_dict = {y: fourier_series(x, f=w, n=125)}
+print(model_dict)
+fit = Fit(model_dict, x=transCurve.wavelength/1E4, y=transCurve.transmission)
+fit_result = fit.execute()
+print(fit_result)
+
+_,axs = plt.subplots()
+axs.plot(transCurve.wavelength/1E4,transCurve.transmission)
+axs.plot(transCurve.wavelength/1E4, fit.model(x=transCurve.wavelength/1E4, **fit_result.params).y,ls=':')
+axs.set_xlabel(r"$\lambda/\mu$m")
+axs.set_ylabel("Transmission/%")
+axs.set_ylim(ymin=0)
+plt.show()
+
+"""
